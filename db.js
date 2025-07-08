f3f60019ba28ac549519fd83c8707bf76efad802
njood8 <njoodi.a55@gmail.com>
2025-07-08 11:08:34 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+const config = {
+  user: 'sa',
+  password: '123456',  // عدلي حسب الباسورد عندك
+  server: 'localhost',
+  database: 'ABSHER',  // بالضبط اسم قاعدة البيانات
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};node index.j
